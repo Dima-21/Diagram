ea19753b1fd28ca686f3b6d6599f6b3106db8ce3
student <student@K09-10>
2018-07-04 17:02:19 +0300

    1.0

---
+++ DiagramEditor/DataDiagram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagramEditor
+{
+    class DataDiagram : IComparable
+    {
+        public String Name { get; set; }
+        public int Procent { get; set; }
+        public DataDiagram(string n, int p)
+        {
+            Name = n;
+            Procent = p;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return (obj as DataDiagram).Procent - Procent;
+        }
+    }
+}
--- DiagramEditor/MainWindow.xaml.cs
+++ DiagramEditor/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace DiagramEditor
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<DataDiagram> lst = new List<DataDiagram>();
+        int start = 600;
+        public double WidthRect{ get; set; }
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            String comb = $"{legend.Text} => {data.Text}";
+            dataList.Items.Add(comb);
+            lst.Add(new DataDiagram(legend.Text, Int32.Parse(data.Text)));
+            legend.Clear();
+            data.Clear();
+            legend.Focus();
+        }
+
+        private void drawRect(double width, double heigth, Brush f, Brush s, double t, double a, double b)
+        {
+            Rectangle r = new Rectangle();
+            r.Width = width;
+            r.Height = heigth;
+            r.Fill = f;
+            r.Stroke = s;
+            r.StrokeThickness = t;
+            r.RenderTransform = new TranslateTransform(a, b);
+            viewBox.Children.Add(r);
+        }
+
+        private void draw_Click(object sender, RoutedEventArgs e)
+        {
+            WidthRect = (viewBox.ActualWidth / lst.Count) - (10* lst.Count);     
+            double x = 830;
+            double y = 660;
+            //lst.Sort();
+            int max = lst.Max(k => k.Procent);
+            double height;
+            foreach (var item in lst)
+            {
+                height = (100 * item.Procent)/max;
+                drawRect(WidthRect, height, Brushes.Orange, Brushes.Blue, 1, x, y- viewBox.ActualHeight);
+                x -= WidthRect+10;
+            }
+        }
+
+        private void del_Click(object sender, RoutedEventArgs e)
+        {
+            int k = dataList.SelectedIndex;
+            if (k < 0)
+                MessageBox.Show("Вы не выбрали элемент для удаления", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Error);
+            else
+                dataList.Items.RemoveAt(k);
+        }
+
+        private void reset_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
